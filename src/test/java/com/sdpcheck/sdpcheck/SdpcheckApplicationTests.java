702ebdc0cb0581b09c4a23325a50d9a3e3beeeab
sangwoolee12 <samkolder0102@gmail.com>
2026-09-19 01:27:03 +0900

    chore: configure backend development environment

--- src/test/java/com/sdpcheck/sdpcheck/SdpcheckApplicationTests.java
+++ src/test/java/com/sdpcheck/sdpcheck/SdpcheckApplicationTests.java
@@ -2,9 +2,21 @@ package com.sdpcheck.sdpcheck;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class SdpcheckApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:17-alpine"));
 
 	@Test
 	void contextLoads() {

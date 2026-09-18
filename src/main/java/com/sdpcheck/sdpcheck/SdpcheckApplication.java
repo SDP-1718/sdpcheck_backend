c1ad196cecb3023940410cae870661729c8b7239
sangwoolee12 <samkolder0102@gmail.com>
2026-09-19 00:51:18 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sdpcheck/sdpcheck/SdpcheckApplication.java
@@ -0,0 +1,13 @@
+package com.sdpcheck.sdpcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdpcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdpcheck/sdpcheck/SdpcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdpcheck.sdpcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
